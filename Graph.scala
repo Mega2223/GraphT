4218c8a58ab3d21abd248a5f93657d1721a004bd
Júlio César B. O. Sampaio <59067466+Mega2223@users.noreply.github.com>
2025-12-26 23:38:03 +0000

    Genesis

---
+++ Graph.scala
@@ -0,0 +1,11 @@
+class Vertex(val label: String){
+
+}
+
+class Edge(val label: String, val b: Vertex, val e: Vertex){
+
+}
+
+class Digraph(val vertices: List[Vertex], val edges: List[Edge]){
+    
+}
